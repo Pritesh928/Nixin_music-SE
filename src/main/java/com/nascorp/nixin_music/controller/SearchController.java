b5939bb278551e90aba73878675a1f8d0e4eedf0
Pritesh <pptsingh123@gmail.com>
2026-06-25 15:28:12 +0530

    feat : searchcontroller

--- src/main/java/com/nascorp/nixin_music/controller/SearchController.java
+++ src/main/java/com/nascorp/nixin_music/controller/SearchController.java
@@ -4,20 +4,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-
-@RequestMapping("api/search")
 @RestController
+@RequestMapping("/api")
 public class SearchController {
-    
-    @GetMapping("id")
-    public String getID(@RequestParam int q){
-        return "Query : " + q;
-    }
 
-    @GetMapping("fname")
-    public String getName(@RequestParam String name){
-        return "Name : " + name;
+    private final RestTemplate restTemplate;
+
+    public SearchController(RestTemplate restTemplate) { 
+        this.restTemplate = restTemplate;
     }
     
+    @GetMapping("/search")
+    public String getQuery(@RequestParam String q){
+
+        String apiKey = "YOUR_YOUTUBE_DATA_API_KEY";
+
+        String youtubeUrl = "https://www.googleapis.com/youtube/v4/search"
+                + "?part=snippet"
+                + "&type=video"
+                + "&maxResults=10"
+                + "&q=" + q
+                + "&key=" + apiKey;
+
+        String response = restTemplate.getForObject(youtubeUrl, String.class);
+
+        return response;
+    }
 }
